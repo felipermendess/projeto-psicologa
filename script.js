c2b6452a6767466777bb2c60d09b215b3c7732d4
felipermendess <felipermendes04@gmail.com>
2024-07-03 23:24:08 -0400

    criação da animação dos links internos

---
+++ script.js
@@ -0,0 +1,15 @@
+const linksInternos = document.querySelectorAll('a[href^="#"]');
+
+function scrollToView(event){
+  event.preventDefault();
+  const href = event.currentTarget.getAttribute('href');
+  const section = document.querySelector(href);
+  section.scrollIntoView({
+    behavior: 'smooth', 
+    block: 'start',
+  });
+}
+
+linksInternos.forEach(link => {
+  link.addEventListener('click', scrollToView);
+})
