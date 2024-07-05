bf2907c60ea195c96217fbfeffd3e7b888873e13
felipermendess <felipermendes04@gmail.com>
2024-07-04 22:34:54 -0400

    otimizacoes 2

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
-const linksInternos = document.querySelectorAll('a[href^="#"]');
+function initScrollIntoView(){
+  const linksInternos = document.querySelectorAll('a[href^="#"]');
 
-function scrollToView(event){
-  event.preventDefault();
-  const href = event.currentTarget.getAttribute('href');
-  const section = document.querySelector(href);
-  section.scrollIntoView({
-    behavior: 'smooth', 
-    block: 'start',
-  });
+  function scrollToView(event){
+    event.preventDefault();
+    const href = event.currentTarget.getAttribute('href');
+    const section = document.querySelector(href);
+    section.scrollIntoView({
+      behavior: 'smooth', 
+      block: 'start',
+    });
+  }
+
+  linksInternos.forEach(link => {
+    link.addEventListener('click', scrollToView);
+  })
 }
-
-linksInternos.forEach(link => {
-  link.addEventListener('click', scrollToView);
-})
+initScrollIntoView();
