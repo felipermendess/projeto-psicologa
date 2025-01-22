eef0965851c00991ae8213ae42a70be59c8010a7
felipermendess <felipermendes04@gmail.com>
2025-01-21 22:40:12 -0400

    mais configurações com eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -14,6 +14,16 @@ export default [
       eqeqeq: "error",
       "space-before-blocks": "error",
       indent: ["error", 2],
+      "keyword-spacing": "error",
+      "no-multiple-empty-lines": "error",
+      "eol-last": ["error", "always"],
+      "no-trailing-spaces": "error",
+      "operator-assignment": "error",
+      "no-inner-declarations": [
+        "error",
+        "functions",
+        { blockScopedFunctions: "disallow" },
+      ],
     },
   },
 ];
--- js/modules/animacao-ao-scroll.js
+++ js/modules/animacao-ao-scroll.js
@@ -1,18 +1,20 @@
 export default function animacaoAoScroll() {
   const sections = document.querySelectorAll(".js-scroll");
-  if (sections.length) {
-    const windowMetade = window.innerHeight * 0.8;
-    function animaScroll() {
-      sections.forEach(section => {
-        const sectionTop = section.getBoundingClientRect().top;
-        const isSection = sectionTop - windowMetade < 0;
-        if (isSection) {
-          section.classList.add("ativo");
-        }
-      });
-    }
-    animaScroll();
+  const windowMetade = window.innerHeight * 0.8;
 
+  function animaScroll() {
+    sections.forEach(section => {
+      const sectionTop = section.getBoundingClientRect().top;
+      const isSection = sectionTop - windowMetade < 0;
+
+      if (isSection) {
+        section.classList.add("ativo");
+      }
+    });
+  }
+
+  if (sections.length) {
+    animaScroll();
     window.addEventListener("scroll", animaScroll);
   }
 }
